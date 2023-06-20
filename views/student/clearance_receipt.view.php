6114fcaa34eaa45027ec6d92c5ccb80a9a864ddf
prince <mirekuprince18@gmail.com>
2023-06-20 20:38:15 +0100

    Modified form

--- views/student/clearance_receipt.view.php
+++ views/student/clearance_receipt.view.php
@@ -1,89 +1,124 @@
-<div class="main-content">
+<div class="content">
     <div class="container-fluid content-top-gap">
         <div class="card" style="border: none !important">
-            <h1 class="text-center mt-3 mb-4"><?= $ctx['student']['institution']['i_name'] ?> Final Year <br> Clearance Form</h1>
-            <div class="card-body">
-                <div class="d-flex mb-2 justify-content-between">
-                    <p>
-                        <b>Student Name:</b> <?= $ctx['student']['fullname'] ?>
-                    </p>
-                    <p>
-                        <b>Student ID:</b> <?= $ctx['student']['index_number'] ?>
-                    </p>
-                    <p>
-                        <b>Class:</b> <?= $ctx['student']['class'] ?>
-                    </p>
+            <div class="justify-content-center text-center">
+                <h2 class="text-center mb-4">
+                    CKT UNIVERSITY FOR OF TECHNOLOGY AND APPLIED SCIENCES
+                </h2>
+                <div class="text-center"> 
+                    <img class="" src="../assets/images/ckt-logo.png" alt="" width="150px">
                 </div>
-                <table class="table table-bordered table-responsive-sm">
-                    <thead>
-                    <tr>
-                        <th>Office</th>
-                        <th>Officer</th>
-                        <th>Status</th>
-                        <th>Remarks</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php if ($ctx['student']['clearance_started']): ?>
-                        <?php foreach ($ctx['clearance_items'] as $clearance_item): ?>
-                            <tr>
-                                <td>
-                                              <span class="fw-bold">
-                                                  <?= $clearance_item['office']['name'] == "Classroom" ? "Class Master" : $clearance_item['office']['name'] ?>
-                                              </span>
-                                </td>
-                                <td>
-                                    <?php echo $clearance_item['officer'] != null ? $clearance_item['officer']['fullname'] : $clearance_item['formmaster']['fullname'] ?>
-                                </td>
-                                <td>
-                                    <?php if ($clearance_item['status'] == "cleared"): ?>
-                                        <b>Cleared</b>
-                                    <?php elseif ($clearance_item['status'] == 'not cleared'): ?>
-                                        <b>
-                                            Not Cleared
-                                        </b>
-                                    <?php else: ?>
-                                        <b>Pending</b>
-                                    <?php endif ?>
-                                </td>
-                                <td>
-                                    <p>
-                                        <?= $clearance_item['remarks'] ?? "N/A" ?>
-                                    </p>
-                                </td>
-                                <td>
-                                    <?= $clearance_item['created_at'] ?>
-                                </td>
-                                <td>
-                                    <?= $clearance_item['time_created'] ?>
-                                </td>
-                            </tr>
-                        <?php endforeach ?>
-                    <?php else: ?>
-                        <tr>
-                            <td colspan='5' class='text-center'>Clearance Not Started</td>
-                        </tr>
-                    <?php endif ?>
-                    </tbody>
-                </table>
-                <div class="mt-5 d-flex justify-content-between">
-                    <p>
-                        Signature: ......................................................
-                    </p>
-                    <p>
-                        Date: ................................................
-                    </p>
+                <p>CLEARANCE FORM</p>
+            </div>
+            
+            <!-- STUDENT INFO TABLE -->
+            <div class="row mt-5 mx-2">
+
+                <div class="col-10 px-0">
+                    <div class="d-flex justify-content-between pn-18">
+                        <p>Rgistration No.: ...........................................................</p>
+                        <p>Student Name: ..............................................................</p>
+                    </div>
+                    <p>Gender: ........................................................................</p>
+                    <div class="d-flex justify-content-between">
+                        <p>Department: .................................................................</p>
+                        <p>Programme: ..................................................................</p>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <p>Signature: ..................................................................</p>
+                        <p>Date: .............................................................................
+                        </p>
+                    </div>
                 </div>
-                <div class="mt-5 d-flex justify-content-between">
-                    <p>
-                        Signature/Stamp: ......................................................
-                    </p>
-                    <p>
-                        Date: ................................................
-                    </p>
+
+                <div class="col-2">
+                    <div class="imgage-holder text-center justify-content-center">
+                        <!-- // FETCH IMAGE HERE -->
+                        Passport Picture
+                    </div>
                 </div>
+
+            </div><hr>
+
+
+            <div class="card-body px-0">
+                
+                <!-- // CONTENT GOES HERE  -->
+
+                <div>
+                    <p>CAMPUS ACCOUNTANT</p>
+                    <P>
+                        I certify that the bearer of this form has paid fully all monie to the university school
+                    </P>
+                    <div class="d-flex mt-4 mb-2 justify-content-between">
+                        <p>Signature/stamp: .............................................</p>
+                        <p>Date: .............................................</p>
+                    </div><br>
+
+                    <p>CAMPUS LIBRARIAN</p>
+                    <P>
+                        I testify that the bearer of this form returned all books, pamphlets
+                        and any other reading materials borrowed from the library or where
+                        penalties were imposed for delay, the bearer has cleared his/her name.
+                    </P>
+                    <div class="d-flex mt-4 mb-2 justify-content-between">
+                        <p>Signature/stamp:  .............................................</p>
+                       <p>Date: .............................................</p>
+                    </div><br>
+
+                    <p>COMPUTER LABORATORY</p>
+                    <P>
+                        I testify that the bearer of this form has fulfilled all his financial
+                        and scholarly obligations to the computer laboratory.
+                    </P>
+                    <div class="d-flex mt-4 mb-2 justify-content-between">
+                        <p>Signature/stamp:  .............................................</p>
+                       <p>Date: .............................................</p>
+                    </div><br>
+
+                    <p>LABORATORY</p>
+                    <P>
+                        The bearer of this form has fulfilled all his financial obligations including
+                        replcacing and repair of broken and damaged laboratory equipment.
+                    </P>
+                    <div class="d-flex mt-4 mb-2 justify-content-between">
+                        <p>Signature/stamp:  .............................................</p>
+                       <p>Date: .............................................</p>
+                    </div><br>
+
+                    <p>SENIOR HALL TUTOR</p>
+                    <P>
+                        The bearer of this form has fulfilled all his financial obligations in
+                        his/her Hall of Residence.
+                    </P>
+                    <div class="d-flex mt-4 mb-2 justify-content-between">
+                        <p>Signature/stamp:  .............................................</p>
+                       <p>Date: .............................................</p>
+                    </div><br>
+
+                    <p>CAMPUS HALL COARCH</p>
+                    <P>
+                        The bearer of this form has submitted all sports equipment in his/her 
+                        possession.
+                    </P>
+                    <div class="d-flex mt-4 mb-2 justify-content-between">
+                        <p>Signature/stamp:  .............................................</p>
+                       <p>Date: .............................................</p>
+                    </div><br>
+
+                    <p>DEAN IN CHARGE</p>
+                    <P>
+                        I certify that the student has discharged all his financial obligations 
+                        to the University school.
+                    </P>
+                    <div class="d-flex mt-4 mb-2 justify-content-between">
+                        <p>Signature/stamp:  .............................................</p>
+                       <p>Date: .............................................</p>
+                    </div><br>
+                </div>
+                
+                <!-- // CONTENT ENDS HERE  -->
+
             </div>
         </div>
     </div>
@@ -91,5 +126,5 @@
 </div>
 
 <script>
-    window.print()
+    // window.print()
 </script>
